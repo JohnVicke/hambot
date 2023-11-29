f41c196af091accf9fb97c1392a0dff883a81ede
Viktor Malmedal <viktor@malmedal.dev>
2023-11-29 10:16:16 +0100

    navbar with image

--- apps/ryze/src/components/navbar/index.tsx
+++ apps/ryze/src/components/navbar/index.tsx
@@ -1,8 +1,21 @@
+import NextImage from "next/image";
+
 import { ThemeToggle } from "./theme-toggle";
 
 export function Navbar() {
   return (
-    <header>
+    <header className="flex justify-between border border-b border-b-border px-8 py-2">
+      <div className="flex items-center space-x-4">
+        <NextImage
+          className="rounded-full"
+          priority
+          src="/assets/hambot.jpeg"
+          alt="hambot"
+          width="40"
+          height="40"
+        />
+        <p className="font-bold">@ham/ryze</p>
+      </div>
       <ThemeToggle />
     </header>
   );
