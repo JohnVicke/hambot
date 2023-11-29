aa2c89f7051ae96c31a20717e48f7fa9feb55445
Viktor Malmedal <viktor@malmedal.dev>
2023-11-29 10:16:12 +0100

    update dashboard components

---
+++ apps/ryze/src/components/dashboard/hambot-api-overview/commands-ran-edit.tsx
@@ -0,0 +1,30 @@
+import { Pen } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+
+export function CommandsRanEdit() {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="secondary"
+            className="inline-flex items-center space-x-1"
+          >
+            <Pen className="mr-2 h-4 w-4" />
+            Edit
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Edit any discrepancies in the commands data.</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
